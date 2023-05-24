91aa92aa33084f8ee9bf01c482212ca33a051aae
Dat Nguyen <dat@duo-studio.co>
2023-05-24 13:53:22 -0400

    allow domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.sanity.io"],
+    domains: ["cdn.sanity.io", "0p77p9q1.apicdn.sanity.io"],
   },
 };
 
